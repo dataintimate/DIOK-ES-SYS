4c4c875b12ac82d83425bfcd5b9b9e14a219dfea
duvindu111 <duvinduthathsara@gmail.com>
2023-12-05 15:21:54 +0530

    111

--- assets/template/templateOne.php
+++ assets/template/templateOne.php
@@ -14,6 +14,9 @@
         a>img{
             margin-bottom: 15px;
         }
+        .td2p{
+            margin-left: 28px;
+        }
         #imgInsta{
             margin-bottom: 0;
         }
@@ -21,36 +24,45 @@
             margin-top: 20px;
         }
         #td2pLast{
-            margin-bottom: 20px;
+            margin-bottom: 14px;
         }
         @media all and (min-width: 320px) and (max-width: 375px){
             #name,#title,#companyName,#email,#mobileNo,#officeNo,#webUrl,#address{
-                font-size: 8px !important;
+                font-size: 6px !important;
+            }
+            .td2p{
+                margin-left: 2px ;
             }
             .example{
                 width: 283px !important;
+            }
+            .line{
+                min-width: 1px !important;
             }
             #side{
                 width: max-content !important;
                 height: 100% !important;
             }
             #selected-image{
-                width: 50px !important;
-                height: 50px !important;
+                width: 40px !important;
+                height: 40px !important;
             }
             p{
-                margin-bottom: 0;
+                margin-bottom: 0 !important;
+            }
+            .td2p>a{
+              font-size: 0px !important;
             }
             a>img{
-                width: 20px !important;
-                height: 20px !important;
+                width: 16px !important;
+                height: 16px !important;
                 margin-bottom: 5px;
             }
             #td2pFirst{
                 margin-top: 0px;
             }
             #td2pLast{
-                margin-bottom: 0px;
+                margin-bottom: 10px;
             }
             .td2p{
                 font-size: 5px !important;
@@ -58,7 +70,13 @@
         }
         @media all and (min-width: 375px) and (max-width: 450px){
             #name,#title,#companyName,#email,#mobileNo,#officeNo,#webUrl,#address{
-                font-size: 9px !important;
+                font-size: 7px !important;
+            }
+            .td2p{
+                margin-left: 6px ;
+            }
+            .line{
+                min-width: 1px !important;
             }
             .example{
                 width: 283px !important;
@@ -68,30 +86,39 @@
                 height: 100% !important;
             }
             #selected-image{
-                width: 50px !important;
-                height: 50px !important;
+                width: 40px !important;
+                height: 40px !important;
             }
             p{
-                margin-bottom: 0;
+                margin-bottom: 1px !important;
+            }
+            .td2p>a{
+                line-height: 8px;
             }
             a>img{
-                width: 20px !important;
-                height: 20px !important;
+                width: 16px !important;
+                height: 16px !important;
                 margin-bottom: 5px;
             }
             #td2pFirst{
                 margin-top: 0px;
             }
             #td2pLast{
-                margin-bottom: 0px;
+                margin-bottom: 8px;
             }
             .td2p{
                 font-size: 5px !important;
             }
         }
         @media all and (min-width: 450px) and (max-width: 500px){
+            .line{
+                width: 4.5px !important;
+            }
+            .td2p{
+                margin-left: 12px ;
+            }
             #name,#title,#companyName,#email,#mobileNo,#officeNo,#webUrl,#address{
-                font-size: 9px !important;
+                font-size: 8px !important;
             }
             .example{
                 width: 350px !important;
@@ -108,7 +135,7 @@
                 height: 50px !important;
             }
             p{
-                margin-bottom: 0;
+                margin-bottom: -2px !important;
             }
             a>img{
                 width: 20px !important;
@@ -127,18 +154,29 @@
         }
         @media all and (min-width: 500px) and (max-width: 600px){
             p{
-                font-size: 14px !important;
-                margin-bottom: 3px;
+                font-size: 10px !important;
+                margin-bottom: 5px;
+            }
+            .td2p{
+                margin-left: 12px ;
+            }
+            .line{
+                width: 1px !important;
+            }
+            .td2p>a{
+                font-size: 10px !important;
             }
             .example{
                 width: 460px !important;
             }
             #side{
-                height: 250px !important;
+                height: 200px !important;
                 width: 130px;
             }
             a>img{
                 margin-bottom: 8px;
+                width: 25px;
+                height: 25px;
             }
             #selected-image{
                 width: 70px !important;
@@ -147,8 +185,11 @@
         }
         @media all and (min-width: 600px) and (max-width: 720px){
             p{
-                font-size: 14px !important;
+                font-size: 11px !important;
                 margin-bottom: 5px;
+            }
+            .td2p>a{
+                font-size: 12px !important;
             }
             .example{
                 width: 550px !important;
@@ -169,23 +210,22 @@
 </head>
 <body>
 <div>
-    <table class="example" cellspacing="0" cellpadding="0" width="650px"
+    <table class="example" cellspacing="0" cellpadding="0" width="652px"
            style="font-family: Arial, sans-serif; border-collapse: collapse;">
         <tr>
             <td class="" id="side" height="280px"  width="200px" style="background-color: #f5f4f4; padding: 7px">
                <span id="selected-image" style="display: block; border-radius: 50%; margin: auto; ">
-
                </span>
             </td>
             <td class="line" style="width: 1px; background-color: #7f5c5c; padding: 0;"></td>
-            <td class="backgroundColor td2" style="position:relative; background-color: #f5f4f4; text-align: left; width: 500px; padding: 10px; padding-left: 30px">
+            <td class="backgroundColor td2" style="position:relative; background-color: #f5f4f4; text-align: left; width: 500px; padding: 10px;">
                 <p class="td2p" id="td2pFirst" style="font-size: 13px; font-weight: bold; color: #7f5c5c;"><span id="name">Mark Mason</span></p>
                 <p class="td2p" style="font-size: 13px; color: #7f5c5c;"><span id="title">Marketing Manager</span></p>
                 <p class="td2p" style="font-size: 13px; color: #7f5c5c;"><span id="companyName">XYZ Corporation</span></p>
-                <p class="td2p"  style="font-size: 13px; color: #7f5c5c;"><span id="address">123 Main Street, Suite 456, Cityville</span></p>
-                <p class="td2p" style=""><a href="mailto:" style="color: #7f5c5c;; text-decoration: none; font-size: 13px;"><span id="email">markmsn@gmail.com</span></a></p>
-                <p class="td2p" style="font-size: 13px; color: #7f5c5c;"><span id="mobileNo">+1 (555) 987-6543</span></p>
-                <p class="td2p" style="font-size: 13px; color: #7f5c5c;"><span id="officeNo">+1 (555) 123-4567</span></p>
+                <p class="td2p"  style="font-size: 13px; color: #7f5c5c; margin-bottom: 3px;"><span id="address">123 Main Street, Suite 456, Cityville</span></p>
+                <p class="td2p" style=""><a href="mailto:" style="color: #7f5c5c; text-decoration: none; font-size: 13px;"><span id="email">markmsn@gmail.com</span></a></p>
+                <p class="td2p" style="font-size: 13px; color: #7f5c5c;"><a style="font-size: 13px"><span id="mobileNo">+1 (555) 987-6543</span></a></p>
+                <p class="td2p" style="font-size: 13px; color: #7f5c5c; margin-bottom: 3px;"><a style="font-size: 13px"><span id="officeNo">+1 (555) 123-4567</span></a></p>
                 <p class="td2p" id="td2pLast" style=""><a style="font-size: 14px; color: #7f5c5c;" href="" target="_blank"><span id="webUrl">www.xyzcorp.com</span></a></p>
             </td>
             <td id="mid" class="backgroundColor"  style="background-color:#f5f4f4; width: 100px;">
