7b0ee7c3219128ea299358bc19c96ba1f58f306e
Ksandeepa07 <kaveensandeepa512@gmail.com>
2023-12-06 11:09:42 +0530

    update

--- assets/template/templateFour.php
+++ assets/template/templateFour.php
@@ -3,21 +3,27 @@
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
+
+        hr{
+            background-color: #15489e !important;
+        }
         .td1p{
             margin-bottom: 0px;
         }
         #selected-image{
             width: 100px !important;
             height: 100px !important;
+            background : url('https://i.postimg.cc/PrQkhXfM/user4.jpg') center/cover no-repeat !important;
         }
         .td2p{
             margin-bottom: 10px;
+            color: #1F365C !important;
         }
         #td2pFirst{
             margin-top: 20px;
         }
         #td2pLast{
-            margin-bottom: 20px;
+            margin-bottom: 10px;
         }
         #td2div{
             margin-left: 20px;
@@ -83,6 +89,7 @@
         @media all and (min-width: 450px) and (max-width: 500px){
             #name,#title,#companyName,#email,#mobileNo,#officeNo,#webUrl,#address{
                 font-size: 9px !important;
+
             }
             .example{
                 width: 350px !important;
@@ -100,6 +107,7 @@
             }
             p{
                 margin-bottom: 0;
+                margin-top: 3px !important;
             }
             a>img{
                 width: 20px !important;
@@ -153,39 +161,61 @@
     <table class="example" cellspacing="0" cellpadding="0" width="675px"
            style="font-family: Arial, sans-serif; border-collapse: collapse; border-radius: 10px;">
         <tr>
-            <td class="backgroundColor td1p" id="side" height="250px"  width="200px" style="background-color: #c1c1c1; padding: 10px;">
-               <span id="selected-image" style="display: block; border-radius: 50%; background-color: white; margin: auto">
-               </span>
-                <p class="td2p" style="font-size: 14px; color: black; margin-top: 10px"><span id="webUrl">www.xyzcorp.com</span></p>
-                <p id="s_mid_icsons" style="display: flex; flex-direction: row; width: max-content; margin: auto">
-                    <a id="linked" href="" style=" margin-right: 7px; font-size: 0px; ">
-                        <!--                            <img  width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/linkedin.png" alt="linkedin"/>-->
-                        <img id="imgLinkedIn" width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/linkedin.png" alt="linkedin"/>
+            <td style="background-color: #1F365C"  >
+                <p style="display:grid; margin: 0 0 0 2px; ">
+                    <a id="linked" href="" style=" margin-top: 7px; font-size: 0px; ">
+                        <img  id="imgLinkedIn" width="25" height="25" src="https://img.icons8.com/ios-filled/30/FFFFFF/linkedin.png" alt="linkedin"/>
                     </a>
-                    <a id="fbook" href="" style=" margin-right: 7px; font-size: 0px; ">
-                        <!--                            <img id="imgFBook" width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/facebook-new.png" alt="facebook-new"/>-->
-                        <img id="imgFBook" width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/facebook--v1.png" alt="facebook--v1"/>
+                    <a id="fbook" href="" style=" margin-top: 7px; font-size: 0px; ">
+                        <img id="imgFBook" width="25" height="25" src="https://img.icons8.com/ios-filled/30/FFFFFF/facebook--v1.png" alt="facebook--v1"/>
                     </a>
-                    <a id="twi" href="" style=" margin-right: 7px; font-size: 0px;">
-                        <!--                            <img id="imgTwitter" width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/twitter.png" alt="twitter"/>-->
-                        <img id="imgTwitter" width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/twitterx--v1.png" alt="twitterx--v1"/>
+                    <a id="twi" href="" style=" margin-top: 7px; font-size: 0px;">
+                        <img id="imgTwitter" width="25" height="25" src="https://img.icons8.com/ios-filled/30/FFFFFF/twitterx--v1.png" alt="twitterx--v1"/>
                     </a>
-                    <a id="insta" href="" style=" font-size: 0px;">
-                        <!--                            <img id="imgInsta" width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/instagram-new.png" alt="instagram-new"/>-->
-                        <img id="imgInsta" width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/instagram-new--v1.png" alt="instagram-new--v1"/>
+                    <a id="insta" href="" style=" font-size: 0px; margin-top: 7px">
+                        <img id="imgInsta" width="25" height="25" src="https://img.icons8.com/ios-filled/30/FFFFFF/instagram-new--v1.png" alt="instagram-new--v1"/>
                     </a>
                 </p>
             </td>
-            <td class="line" style="width: 1px; background-color: #000000"></td>
-            <td class="backgroundColor td2" style="position:relative; background-color: #c1c1c1; text-align: left; width: 500px; padding: 10px;">
+
+            <td align="center" class="backgroundColor td1p" id="side" height="250px"  width="150px" style="background-color:  #1F365C; padding: 10px;   ">
+               <span  id="selected-image" style="display: block; border-radius: 50%; background-color: white;">
+               </span>
+
+
+            </td>
+<!--            <td class="line" style="width: 1px; background-color: #000000"></td>-->
+            <td class="backgroundColor td2" style="position:relative; background-color: #ffffff; text-align: left; width: 500px; padding: 10px; vertical-align: top">
                 <div id="td2div">
-                    <p class="td2p" id="td2pFirst" style="font-size: 14px; font-weight: bold; color: black"><span id="name">Mark Mason</span></p>
-                    <p class="td2p" style="font-size: 14px; color: black"><span id="title">Marketing Manager</span></p>
+
+                        <p class="td2p" id="td2pFirst" style="font-size: 14px; font-weight: bold; color: black"><span id="name">Mark Mason</span></p>
+                        <p class="td2p" style="font-size: 14px; color: black"><span id="title">Marketing Manager</span></p>
+
+
+
+                   <hr style="height: 1px">
+
                     <p class="td2p" style="font-size: 14px; color: black"><span id="companyName">XYZ Corporation</span></p>
-                    <p class="td2p" style="font-size: 14px; color: black"><a href="mailto:youremail@example.com" style="color: #000000; text-decoration: none;"><span id="email">markmsn@gmail.com</span></a></p>
+                    <p class="td2p" style="font-size: 14px; color: black"><a href="mailto:youremail@example.com" style="color: #1F365C; text-decoration: none;"><span id="email">markmsn@gmail.com</span></a></p>
                     <p class="td2p" style="font-size: 14px; color: black"><span id="address">123 Main Street, Suite 456, Cityville</span></p>
                     <p class="td2p" style="font-size: 14px; color: black"><span id="mobileNo">+1 (555) 987-6543</span></p>
                     <p class="td2p" id="td2pLast" style="font-size: 14px; color: black"><span id="officeNo">+1 (555) 123-4567</span></p>
+                    <p class="td2p" style="font-size: 14px; color: black; margin-top: 10px"><span id="webUrl">www.xyzcorp.com</span></p>
+                    <!--                <p id="s_mid_icsons" style="display: flex; flex-direction: row; width: max-content; margin: auto">-->
+                    <!--                    <a id="linked" href="" style=" margin-right: 7px; font-size: 0px; ">-->
+                    <!--                        <img id="imgLinkedIn" width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/linkedin.png" alt="linkedin"/>-->
+                    <!--                    </a>-->
+                    <!--                    <a id="fbook" href="" style=" margin-right: 7px; font-size: 0px; ">-->
+                    <!--                        <img id="imgFBook" width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/facebook--v1.png" alt="facebook--v1"/>-->
+                    <!--                    </a>-->
+                    <!--                    <a id="twi" href="" style=" margin-right: 7px; font-size: 0px;">-->
+                    <!--                        <img id="imgTwitter" width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/twitterx--v1.png" alt="twitterx--v1"/>-->
+                    <!--                    </a>-->
+                    <!--                    <a id="insta" href="" style=" font-size: 0px;">-->
+                    <!--                        <img id="imgInsta" width="30" height="30" src="https://img.icons8.com/ios-filled/30/FFFFFF/instagram-new--v1.png" alt="instagram-new--v1"/>-->
+                    <!--                    </a>-->
+                    <!--                </p>-->
+
                 </div>
             </td>
 
